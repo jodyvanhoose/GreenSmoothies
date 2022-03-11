67f0df6e11fe145355cb8b30554e9fd1a6e635cd
Jody VanHoose <jodyvanhoose80@gmail.com>
2022-03-10 22:41:38 -0500

    Added function to close hamburger menu

--- app.js
+++ app.js
@@ -3,9 +3,18 @@
 const toggleButton = document.getElementById('hamburger');
 const navbarLinks = document.getElementById('navbar-links');
 
+
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-});
+    navbarLinks.classList.toggle('active')
+  });
+
+
+function closeMenu(){
+  navbarLinks.classList.toggle('active');
+}
+
+
+
 
 
 
