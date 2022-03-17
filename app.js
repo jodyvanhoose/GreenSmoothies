9edf6056132863b62f06f0bfbe230961b0049077
Jody VanHoose <jodyvanhoose80@gmail.com>
2022-03-17 07:26:46 -0400

    Added favicon and animation for smoothie generator button

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 const toggleButton = document.getElementById('hamburger');
 const navbarLinks = document.getElementById('navbar-links');
 
-
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
   });
-
 
 function closeMenu(){
   navbarLinks.classList.toggle('active');
@@ -15,6 +13,25 @@ function closeMenu(){
 
 
 
+
+
+// Smoothie Generator
+
+let menuItems = ['Spinach Special', 'Banana Bomber', 'The Appalachian', 'Avocado Action', 'Columbia Gold', 'Big Blue Madness', 'Strawberry Smash', 'Mixed Berry Banaza', 'Mango Madness', 'Mint Cucumber'];
+
+const smoothieBtn = document.getElementById('smoothie-btn');
+const showSmoothie = document.getElementById('show-selection')
+
+// function pickSmoothie(){
+//   let selection = Math.floor(Math.random() * menuItems.length);
+//   const showSmoothie = document.getElementById('show-selection')
+//   showSmoothie.innerText = 'Try \'' + menuItems[selection] + '\' you\'ll love it!';
+// }
+
+smoothieBtn.addEventListener('click', () =>{
+  let selection = Math.floor(Math.random() * menuItems.length);
+  showSmoothie.innerText = `Try '${menuItems[selection]}' you'll love it!`;
+});
 
 
 
@@ -25,7 +42,6 @@ const submitBtn = document.getElementById('btn');
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-
   submitBtn.innerText = 'Submitted!';
 });
 
@@ -33,26 +49,6 @@ submitBtn.addEventListener('click', (e) => {
 
 
 
-// Smoothie Generator
 
-let smoothies = ['Spinach Special', 'Banana Bomber', 'The Appalachian', 'Avocado Action', 'Columbia Gold', 'Big Blue Madness', 'Strawberry Smash', 'Mixed Berry Banaza', 'Mango Madness', 'Mint Cucumber'];
-
-const smoothieBtn = document.getElementById('smoothie-btn');
   
-const showSmoothie = document.getElementById('show-selection')
-
-
-
-// function pickSmoothie(){
-//   let selection = Math.floor(Math.random() * smoothies.length);
-//   const showSmoothie = document.getElementById('show-selection')
-//   showSmoothie.innerText = 'Try \'' + smoothies[selection] + '\' you\'ll love it!';
-// }
-
-smoothieBtn.addEventListener('click', () =>{
-  let selection = Math.floor(Math.random() * smoothies.length);
-  showSmoothie.innerText = 'Try \'' + smoothies[selection] + '\' you\'ll love it!'
-});
-  
-
 
