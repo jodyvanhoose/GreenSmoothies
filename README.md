52a761cef1e1357ce306dde452cdef45e1fb94e7
Jody VanHoose <jodyvanhoose80@gmail.com>
2022-03-21 21:49:11 -0400

    javascript for validation, animation for blender buuton, and finishing touches to readme

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 # GreenSmoothies
 Author: Jody VanHoose
 ## About
-Green Smoothies is a fictional website I created for a course in the boot camp Code Kentucy in which I'm enrolled. The website is for Green Smoothies, a fictional smoothie store that specializes in healthy options. It was developed with mobile first in mind. 
+Green Smoothies is a fictional website I created for a course in the boot camp [Code Kentucky](https://codekentucky.org/) in which I'm enrolled. The website is for Green Smoothies, a fictional smoothie store that specializes in healthy options. It was developed with mobile first in mind. 
 ### Key Features
 * Clean simple design
 * Mobile first approach
 * Responsive desgin
-* Random smootie Generator
+* Random Smoothie Generator
 ___
 ## Technologies/Languages
 * HTML 5
@@ -19,10 +19,12 @@ ___
 * Responsive navigation menu with hamburger menu at mobile size and expands for desktop size
 * Flexbox
 * CSS Grid
+* Smooth scrolling for internal site navigation
 * :nth-of-type element
 * Buttons and Links with hover states
 * Box shadows
 * Transition and Transform properties
+* Animations with keyframes
 
 ### Media Queries
 * 3 media queries were utilized
@@ -30,16 +32,18 @@ ___
     * 840px
     * 1100px
 ### Javascript
-* Creating variables (6)
+* Creating Variables
+* Creating an Array
+* Creating Objects
 * Hamburger menu
   * Created functions to open/close menu
-* Change Submit button
-  * Utilized preventDefault() function to stop reload of page when submit button clicked
-  * Changed submit button's inner text to \'Submitted\' when clicked
 * Random Smoothie generator program
-  * Created function to show random smoothie for user when button is clicked
-    * Created an array and populated with smoothie items.
+  * Created function to show random smoothie for user when animated blender button is clicked "blending"
+    * Created an array and populated with objects containing smoothie items and smoothie prices.
     * Utilized Math.floor() and Math.random() functions to pull random items from array
-    * Shows array item (smoothie item) on page below button when button is clicked
-
-
+    * Shows array item (smoothie item & smoothie price) on page below button when button is clicked
+* Form validation
+  * Utilizing javascript checkValidity() function to validate for first and last name as well as email address
+    * Set custom message and styling utilizing setCustomValidity() function
+  * Utilized preventDefault() function to stop reload of page when submit button clicked
+  * Change submit button to text \'Submitted\' when clicked
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// hamburger toggle
+
+
+// hamburger menu toggle
 
 const toggleButton = document.getElementById('hamburger');
 const navbarLinks = document.getElementById('navbar-links');
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
   });
 
 function closeMenu(){
@@ -14,41 +16,107 @@ function closeMenu(){
 
 
 
-
 // Smoothie Generator
 
-let menuItems = ['Spinach Special', 'Banana Bomber', 'The Appalachian', 'Avocado Action', 'Columbia Gold', 'Big Blue Madness', 'Strawberry Smash', 'Mixed Berry Banaza', 'Mango Madness', 'Mint Cucumber'];
+let menuItems = [
+  {item: 'Spinach Special', price: 5.59},
+  {item: 'Banana Bomber', price: 5.99},
+  {item: 'The Appalachian', price: 6.59},
+  {item: 'Avocado Action', price: 5.99},
+  {item: 'Columbia Gold', price: 7.99},
+  {item: 'Big Blue Madness', price: 5.59},
+  {item: 'Strawberry Smash', price: 4.99},
+  {item: 'Mixed Berry Banaza', price: 4.99},
+  {item: 'Mango Madness', price: 4.99},
+  {item: 'Mint Cucumber', price: 4.99}
+];
 
 const smoothieBtn = document.getElementById('smoothie-btn');
-const showSmoothie = document.getElementById('show-selection')
-
-// function pickSmoothie(){
-//   let selection = Math.floor(Math.random() * menuItems.length);
-//   const showSmoothie = document.getElementById('show-selection')
-//   showSmoothie.innerText = 'Try \'' + menuItems[selection] + '\' you\'ll love it!';
-// }
+const showSmoothie = document.getElementById('show-selection');
 
 smoothieBtn.addEventListener('click', () =>{
   let selection = Math.floor(Math.random() * menuItems.length);
-  showSmoothie.innerText = `Try '${menuItems[selection]}' you'll love it!`;
+  showSmoothie.innerText = `Try '${menuItems[selection].item}' you'll love it!
+  $${menuItems[selection].price}`;
 });
 
 
 
 
-// submit button success
+// form validation/submit button success
 
-const submitBtn = document.getElementById('btn');
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  submitBtn.innerText = 'Submitted!';
+const submitBtn = document.querySelector('#submit');
+const firstCheck = document.querySelector('#first-check');
+const firstX = document.querySelector('#first-x');
+const lastCheck = document.querySelector('#last-check');
+const lastX = document.querySelector('#last-x');
+const emailCheck = document.querySelector('#email-check');
+const emailX = document.querySelector('#email-x');
+
+
+const fName = document.querySelector('input');
+
+fName.addEventListener('input', () =>{
+  fName.setCustomValidity('');
+  fName.checkValidity();
+  console.log(fName.checkValidity());
+  if(fName.validity.valid == true){
+    fName.style.borderColor = '#26Ad1a';
+    firstCheck.style.display = 'inline';
+    firstX.style.display = 'none';
+  }
 });
 
 
+fName.addEventListener('invalid', () => {
+  fName.setCustomValidity('My smoothie has a first name...');
+  fName.style.borderColor = 'red';
+  firstX.style.display = 'inline';
+  firstCheck.style.display = 'none';
+});
+
+const lastName = document.querySelector('#last-name');
+
+lastName.addEventListener('input', () =>{
+  lastName.setCustomValidity('');
+  lastName.checkValidity();
+  console.log(lastName.checkValidity());
+  if(lastName.validity.valid == true){
+    lastName.style.borderColor = '#26Ad1a';
+    lastCheck.style.display = 'inline';
+    lastX.style.display = 'none';
+  }
+});
+
+lastName.addEventListener('invalid', () => {
+  lastName.setCustomValidity('My smoothie has a second name...');
+  lastName.style.borderColor = 'red';
+  lastX.style.display = 'inline';
+  lastCheck.style.display = 'none';
+});
+
+const email = document.querySelector('#email');
+
+email.addEventListener('input', () =>{
+  email.setCustomValidity('');
+  email.checkValidity();
+  console.log(email.checkValidity());
+  if(email.validity.valid == true){
+    email.style.borderColor = '#26Ad1a';
+    emailCheck.style.display = 'inline';
+    emailX.style.display = 'none';
+    submitBtn.addEventListener('click', () => {
+      submitBtn.textContent = 'Submitted';
+    });
+  }
+});
 
 
-
-
-  
+email.addEventListener('invalid', () => {
+  email.setCustomValidity(`You can't get awesome smoothie coupons without a valid email address`);
+  email.style.borderColor = 'red';
+  emailX.style.display = 'inline';
+  emailCheck.style.display = 'none';
+});
 
